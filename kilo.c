5bbbb49decf865f5f399b749058ca006512db5cc
yashasvi-goel <yashasvigoel@gmail.com>
2020-01-20 17:23:29 +0530

    adding

--- kilo.c
+++ kilo.c
@@ -22,19 +22,29 @@ void enableRawMode(){
 	raw.c_iflag&=~(IXON|ICRNL|INPCK|ISTRIP|BRKINT);//input flags
 	//IXON disables ctl+s ctl+q
 	raw.c_oflag&= ~(OPOST);//OPOST disables all post-processing including carriage operator
+	//Following are to edit config of read()
+	raw.c_cc[VMIN]=0;//min chars after which read returns
+	raw.c_cc[VTIME]=10;//max time read() waits before returning
+
 
 	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);//apply the attrs(file,delay,which one);
 }
 int main(){
 	enableRawMode();
-	char c;
-	while(read(STDIN_FILENO,&c,1)==1&&c!='q'){
-		if(iscntrl(c)){
-			printf("%d\r\n",c);
+	//char c;
+	while(1){
+		char c='\0';
+		read(STDIN_FILENO,&c,1);
+		if(c!='q'){
+			if(iscntrl(c)){
+				printf("%d\r\n",c);
+			}
+			else{
+				printf("%d '%c'\r\n",c,c);
+			}
 		}
-		else{
-			printf("%d '%c'\r\n",c,c);
-		}
+		else
+			break;
 	}
 	return 0;
 }
