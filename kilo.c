79921b79c72a2bf7cd3fb92199074b976e999b3e
yashasvi-goel <yashasvigoel@gmail.com>
2020-01-21 21:40:06 +0530

    adding

--- kilo.c
+++ kilo.c
@@ -4,31 +4,48 @@
 #include<stdlib.h>
 #include<stdio.h>
 #include<ctype.h>
+#include<string.h>
 
 #define ctrl(k) ((k) & 0x1f)
+#define str_INIT {NULL,0}
 
-struct editorConfig{
+typedef struct editorConfig{
 	int screenRows;
 	int screenColumns;
 	struct termios orig_termios;
-};
-
+}editorConfig;
+typedef struct strBuffer{
+	char *buffer;
+	int len;
+}strBuffer;
+void bufFree(strBuffer *var){
+	free(var->buffer);
+}
+void bufAppend(strBuffer* ab,const char *s,int len){
+	char *new=realloc(ab->buffer,ab->len+len);
+	if(new==NULL)
+		return;
+	memcpy(&new[ab->len],s,len);
+	ab->buffer=new;
+	ab->len+=len;
+}
 struct editorConfig E;
 void clearScreen();
 char readKey();
+int cursorPosition(int*,int*);
 void die(const char *s)
 {
 	clearScreen(0);
 	perror(s);
 	exit(1);
 }
-int getWindowSize(int *rows, int *cols){
+int getWindowSize(int *rows, int *cols)
+{
   struct winsize ws;
   if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0){
 	  if(write(STDOUT_FILENO,"\x1b[500C\x1b[600B",12)!= 12)
 		  return -1;
-	  readKey();
-    return -1;
+	  return cursorPosition(rows,cols);
   }
   else{
     *cols = ws.ws_col;
@@ -88,33 +105,47 @@ void processKeypress()//manages all the editor modes and special characters
 			break;
 	}
 }
-void drawTildes()//draws sides
+void drawTildes(strBuffer* ab)//draws tildes
 {
 	int y;
 	for(y=0;y<E.screenRows;y++)
-		write(STDOUT_FILENO,"~\r\n",3);
+	{
+		bufAppend(ab,"~",1);
+//		write(STDOUT_FILENO,"~",1);
+		if(y<E.screenRows-1)
+			bufAppend(ab,"\r\n",2);
+//			write(STDOUT_FILENO,"\r\n",3);
+	}
 }
-void clearScreen(int options){
-	write(STDOUT_FILENO,"\x1b[2J",4);
-	write(STDOUT_FILENO,"\x1b[H",3);
+void clearScreen(int options)
+{
+	strBuffer ab=str_INIT;
+	bufAppend(&ab,"\x1b[?25l",6);
+	bufAppend(&ab,"\x1b[2J",4);
+	bufAppend(&ab,"\x1b[H",3);
 	if(options==1)
-		drawTildes();
-	write(STDOUT_FILENO, "\x1b[H",3);
+		drawTildes(&ab);
+	bufAppend(&ab,"\x1b[H",3);
+	bufAppend(&ab,"\x1b[?25l",6);
+
+	write(STDOUT_FILENO,ab.buffer,ab.len);
+	bufFree(&ab);
 }
 int cursorPosition(int* rows,int* cols)
 {
+	char buf[32];
+	unsigned int i=0;
 	if(write(STDOUT_FILENO,"\x1b[6n",4)!=4)
 		return -1;
-	printf("\r\n");
-	char c;
-	while(read(STDIN_FILENO,&c,1)==1){
-		if(ctrl(c))
-			printf("%d\r\n",c);
-		else
-			printf("%d ('%c')\r\n", c, c);
+	while(i<sizeof(buf) -1){
+		if(read(STDIN_FILENO,&buf[i],1)==1) break;
+		if(buf[i]=='R') break;
+		i++;
 	}
-	readKey();
-	return -1;
+	buf[i]='\0';
+	if(buf[0]!='\x1b' || buf[1]!='[') return -1;
+	if(sscanf(&buf[2], "%d;%d",rows,cols)!=2) return -1;
+	return 0;
 }
 int main(){
 	enableRawMode();
